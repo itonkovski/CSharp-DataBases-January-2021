eb5ff8564fb01c4ce5861301cc2cfba6c2318fc5
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-02-02 11:50:05 +0100

    03Update
    Upload

---
+++ Databases-MSSQL-Server-Retake-Exam - 11 August 2020/03.Update.sql
@@ -0,0 +1,17 @@
+UPDATE Ingredients
+    SET DistributorId = 35
+    WHERE Name = 'Bay Leaf'
+
+UPDATE Ingredients
+SET DistributorId = 35
+WHERE Name = 'Paprika'
+
+UPDATE Ingredients
+SET DistributorId = 35
+WHERE Name = 'Poppy'
+
+UPDATE Ingredients
+SET OriginCountryId = 14
+WHERE OriginCountryId = 8
+
+
