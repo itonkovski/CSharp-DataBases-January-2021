816aaf6986160790a7a1188b5913159851cc00fe
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-02-10 09:50:44 +0100

    Task10
    Upload

---
+++ Databases-MSSQL-Server-Retake-Exam - 11 August 2020/10.CountryRepresentative.sql
@@ -0,0 +1,16 @@
+SELECT CountryName,
+       DistributorName
+FROM
+    (
+        SELECT c.Name AS CountryName,
+               d.Name AS DistributorName,
+               COUNT(i.DistributorId) AS IngredientsByDistributor,
+               DENSE_RANK() OVER (PARTITION BY c.Name ORDER BY COUNT(i.DistributorId) DESC) AS Rank
+        FROM Countries AS c
+                 JOIN Distributors AS d ON c.Id = d.CountryId
+                 JOIN Ingredients AS i ON d.Id = i.DistributorId
+        GROUP BY c.Name, d.Name
+    ) AS Ranked
+WHERE Rank = 1
+ORDER BY CountryName,
+         DistributorName 
