5d890c9732407798a104691c712732a4a742b5a2
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-02-12 16:13:20 +0100

    05.EEE-Mails
    Upload

---
+++ DatabasesMSSQLServerExam-21Jun2020/05.EEE-Mails.sql
@@ -0,0 +1,9 @@
+SELECT a.FirstName,
+       a.LastName,
+       FORMAT(a.BirthDate, 'MM-dd-yyyy'),
+       c.Name,
+       a.Email
+    FROM Accounts AS a
+    JOIN Cities AS c ON c.Id = a.CityId
+    WHERE Email LIKE 'e%'
+ORDER BY c.Name
