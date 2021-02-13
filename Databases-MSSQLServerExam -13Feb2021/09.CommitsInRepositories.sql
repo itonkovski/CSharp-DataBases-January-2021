371e271a1f4c1c5351959883ed26e1e67a2a1d26
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-02-13 09:49:14 +0100

    09.CommitsInRepos
    Upload

---
+++ Databases-MSSQLServerExam -13Feb2021/09.CommitsInRepositories.sql
@@ -0,0 +1,10 @@
+SELECT TOP(5) r.Id,
+       r.Name,
+       COUNT(c.Id) AS Commits
+    FROM Repositories AS r
+    JOIN RepositoriesContributors AS rc ON r.Id = rc.RepositoryId
+    JOIN Commits AS c ON r.Id = c.RepositoryId
+GROUP BY r.Id, r.Name
+ORDER BY Commits DESC,
+         r.Id,
+         r.Name
