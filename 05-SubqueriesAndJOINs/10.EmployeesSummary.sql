4f655b91f0a6f5e5aa869f5514f944790b5f5189
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-02-23 13:19:14 +0100

    Task10
    Upload

---
+++ 05-SubqueriesAndJOINs/10.EmployeesSummary.sql
@@ -0,0 +1,9 @@
+SELECT TOP 50
+    e.EmployeeID,
+    e.FirstName + ' ' + e.LastName AS EmployeeName,
+    m.FirstName + ' ' + m.LastName AS ManagerName,
+    d.Name AS DepartmentName
+    FROM Employees AS e
+    INNER JOIN Employees AS m ON m.EmployeeID = e.ManagerID
+    INNER JOIN Departments AS d on e.DepartmentID = d.DepartmentID
+ORDER BY e.EmployeeID
