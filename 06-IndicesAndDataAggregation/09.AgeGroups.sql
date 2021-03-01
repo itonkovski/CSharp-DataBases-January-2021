4abec5d2e22870ee4adbc38b43ecad8559825ea1
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-01 12:10:34 +0100

    Task09
    Upload

---
+++ 06-IndicesAndDataAggregation/09.AgeGroups.sql
@@ -0,0 +1,15 @@
+SELECT a.AgeGroup, COUNT(*) AS WizardCount FROM
+	(SELECT 
+		CASE
+		WHEN Age <= 10 THEN '[0-10]'
+		WHEN Age <= 20 THEN '[11-20]'
+		WHEN Age <= 30 THEN '[21-30]'
+		WHEN Age <= 40 THEN '[31-40]'
+		WHEN Age <= 50 THEN '[41-50]'
+		WHEN Age <= 60 THEN '[51-60]'
+		ELSE '[61+]'
+		END AS AgeGroup
+		FROM WizzardDeposits) AS a
+	GROUP BY a.AgeGroup
+	ORDER BY a.AgeGroup
+
