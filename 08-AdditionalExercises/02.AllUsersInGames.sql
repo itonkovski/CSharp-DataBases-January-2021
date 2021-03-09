a2136c671491b80484271a19c45708604380d723
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-09 13:42:24 +0100

    Task02
    Upload

---
+++ 08-AdditionalExercises/02.AllUsersInGames.sql
@@ -0,0 +1,14 @@
+SELECT g.Name AS [Game],
+       gt.Name AS [Game Type],
+       u.Username,
+       ug.Level,
+       ug.Cash,
+       c.Name AS [Character]
+    FROM Games AS g
+    JOIN GameTypes AS gt ON g.GameTypeId = gt.Id
+    JOIN UsersGames AS ug ON g.Id = ug.GameId
+    JOIN Users AS u ON ug.UserId = u.Id
+    JOIN Characters AS c ON c.Id = ug.CharacterId
+    ORDER BY ug.Level DESC,
+             u.Username,
+             g.Name
