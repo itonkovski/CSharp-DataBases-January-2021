3a7143a7065214c077b7ef5f3bfcb637aaccf58c
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-09 13:41:57 +0100

    Task01
    Upload

---
+++ 08-AdditionalExercises/1.NumberOfUsersForEmailProvider.sql
@@ -0,0 +1,6 @@
+SELECT SUBSTRING(u.Email,CHARINDEX('@',u.Email) + 1, LEN(u.Email)) AS [Email Provider],
+       COUNT(SUBSTRING(u.Email, CHARINDEX('@', u.Email) + 1, LEN(u.Email))) AS [Number Of Users]
+    FROM Users AS u
+    GROUP BY SUBSTRING(u.Email,CHARINDEX('@',u.Email) + 1, LEN(u.Email))
+    ORDER BY [Number Of Users] DESC,
+             [Email Provider]
