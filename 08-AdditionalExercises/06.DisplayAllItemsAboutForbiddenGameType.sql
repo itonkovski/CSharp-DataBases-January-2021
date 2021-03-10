c581d8483660c4b44131afe2f017d896ee62ad1c
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-03-10 11:22:12 +0100

    Task06
    Upload

---
+++ 08-AdditionalExercises/06.DisplayAllItemsAboutForbiddenGameType.sql
@@ -0,0 +1,9 @@
+SELECT i.Name AS [Item],
+       i.Price,
+       i.MinLevel,
+       gt.Name AS [Forbidden Game Type]
+    FROM Items AS i
+    LEFT JOIN GameTypeForbiddenItems AS gtfi ON i.Id = gtfi.ItemId
+    LEFT JOIN GameTypes AS gt ON gt.Id = gtfi.GameTypeId
+ORDER BY gt.Name DESC,
+         [Item]
